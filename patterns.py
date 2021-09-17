1421e85ae6f445c1ba404c68c5c28d3993a0a0ce
Adebisi Ahmed <adebisi.ahmed10@gmail.com>
2021-09-17 09:01:19 +0100

    aaded more patterns

--- patterns.py
+++ patterns.py
@@ -129,6 +129,50 @@ def pattern13(n):
     print()
 
 
+def pattern17(n):
+  print("\npattern 17 \n")
+  for row in range(0,  2 * n):
+    # spaces
+    spaceLength = n - row if row < n else row - n + 2
+    for s in range(0, spaceLength):
+      print(" ",end =" ")
+    colLength = row if row < n else n - (row - n + 2);
+    for col in range(colLength, -1, -1):
+      print(col + 1, end =" ")
+    for col in range(2, colLength + 2):
+      print(col, end =" ")
+    print()
+
+def pattern20(n):
+  print("\npattern 20 \n")
+  for row in range(0, n):
+    colLength = n
+    for col in range(0, colLength):
+      string = '*'
+      if not (row == 0 or row == n-1) and not (col == 0 or col == colLength - 1):
+        string = ' '
+      print(string, end='')
+    print()
+
+def pattern21(n):
+  print("\npattern 21 \n")
+  lastEnd, length = -1, 1
+  for row in range(0, n):
+    for col in range(lastEnd + 1, length):
+      print(col + 1, end =" ")
+    lastEnd = col
+    length = lastEnd + 1 + row + 2
+    print()
+
+def pattern22(n):
+  print("\npattern 22 \n")
+  lastValue = 1
+  for row in range(0, n):
+    lastValue =  1 if row % 2 == 0 else 0
+    for col in range(0, row + 1):
+      print(lastValue, ' ', end='')
+      lastValue = 1 if lastValue == 0 else 0
+    print()
 
 pattern1(5)
 pattern2(5)
@@ -142,5 +186,7 @@ pattern9(5)
 pattern10(5)
 pattern11(5)
 pattern12(5)
-
-      
+pattern17(5)
+pattern20(5)
+pattern21(5)
+pattern22(5)
